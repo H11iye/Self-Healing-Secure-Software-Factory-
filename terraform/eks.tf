b1f5985ef822eb8cfbbb88a53360764a93fc8d50
H11iye <hamdi.iyed11@gmail.com>
2026-02-10 10:42:11 +0000

    create an KMS key and add encryption_config block to aws_eks_cluster resource

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "eks" {
+  description = "EKS Cluster Secrets Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation = true # Security best practice to rotate keys annually; AWS handles this automatically when enabled
+}
 resource "aws_eks_cluster" "main" {
   name     = "secure-factory-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -9,10 +14,19 @@ resource "aws_eks_cluster" "main" {
     endpoint_public_access  = true # Set to false later for extreme security
   }
 
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = aws_kms_key.eks.arn
+    }
+  }
+
   enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
 }
+
+
 
 # IAM Role for the EKS Cluster itself
 resource "aws_iam_role" "eks_cluster_role" {
